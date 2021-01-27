e0e9deff99dd0a933cdc80e1cf71aaefe13f5b9e
Faisal S <sfaisalawan84@gmail.com>
2021-01-27 14:23:38 +0000

    Initial Commit

---
+++ Export.ps1
@@ -0,0 +1,15 @@
+$Groups = Get-MsolGroup
+[array]$GroupMembers = $null
+foreach ($Group in $Groups)
+{
+    $Members = Get-MsolGroupMember -GroupObjectId $Group.ObjectId
+    foreach($Member in $Members)
+    {
+        $GroupMember = New-Object -TypeName psobject
+        $GroupMember | Add-Member NoteProperty -Name GroupName -Value $Group.DisplayName
+        $GroupMember | Add-Member NoteProperty -Name GroupEmail -Value $Group.EmailAddress
+        $GroupMember | Add-Member NoteProperty -Name MemberName -Value $Member.DisplayName
+        $GroupMembers += $GroupMember
+    }
+}
+$GroupMembers | Export-Csv Members.csv
